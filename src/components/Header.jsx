3d31aadcb1267cf3534096523b404d3a8857542f
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-01-11 15:50:47 +0200

    feat:add logic of number of  cart items in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import { CartContext } from "../store/CartContext";
 
 function Header() {
+  const ctx = useContext(CartContext);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +12,9 @@ function Header() {
         <h1>Food Order App</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>
+          Cart ({ctx.items.reduce((total, item) => total + item.quantity, 0)})
+        </Button>
       </nav>
     </header>
   );
