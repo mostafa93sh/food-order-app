90eccb8b5f18c97ebcb07426c665e9f90bc05838
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-01-12 02:46:34 +0200

    feat: creat userProgressContext.jsx which indicate when to open cart modal or check out modal

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "",
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export const UserProgressContextProvider = ({ children }) => {
+  const [userProgress, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+  function hideCart() {
+    setUserProgress("");
+  }
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart: showCart,
+    hideCart: hideCart,
+    showCheckout: showCheckout,
+    hideCheckout: hideCheckout,
+  };
+
+  return (
+    <UserProgressContext.Provider value={userProgressCtx}>
+      {children}
+    </UserProgressContext.Provider>
+  );
+};
+
+export default UserProgressContext;
