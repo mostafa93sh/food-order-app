794cc367b276b423357ca6cceaaa6359bb5c31e3
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-01-12 02:48:05 +0200

    feat:add and consume the user progress context in the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import { CartContext } from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 function Header() {
   const ctx = useContext(CartContext);
+  const progressCtx = useContext(UserProgressContext);
   return (
     <header id="main-header">
       <div id="title">
@@ -12,7 +14,7 @@ function Header() {
         <h1>Food Order App</h1>
       </div>
       <nav>
-        <Button textOnly>
+        <Button textOnly onClick={progressCtx.showCart}>
           Cart ({ctx.items.reduce((total, item) => total + item.quantity, 0)})
         </Button>
       </nav>
