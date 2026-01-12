62893731436c30bced83026a5c2fbfce6f803b66
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-01-12 03:46:10 +0200

    feat: add Input.jsx

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Input({ id, label, ...props }) {
+  return (
+    <p className="control">
+      <label htmlFor={id}>{label}</label>
+      <input name={id} id={id} required {...props} />
+    </p>
+  );
+}
+
+export default Input;
