ba19ca939ddae0376371f6ae4e0743cdaf2afc0c
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-01-12 20:38:40 +0200

    feat : add handleSubmit function in Checkout.jsx components

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -17,23 +17,32 @@ function Checkout() {
   function handleCancel() {
     UserProgressCtx.hideCheckout();
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+  }
+
   return (
     <Modal isOpen={UserProgressCtx.progress === "checkout"}>
-      <h2>Checkout</h2>
-      <p className="cart-total">Total Amount: ${cartTotal}</p>
-      <Input id="Full-Name" label="Full Name" type="text" />
-      <Input id="email" label="Email Address" type="email" />
-      <Input id="Street" label="Street" type="text" />
-      <div className="control-row">
-        <Input id="City" label="City" type="text" />
-        <Input id="Postal-Code" label="Postal Code" type="text" />
-      </div>
-      <p className="modal-actions">
-        <Button type="button" textOnly onClick={handleCancel}>
-          Cancel
-        </Button>
-        <Button>Confirm</Button>
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p className="cart-total">Total Amount: ${cartTotal}</p>
+        <Input id="Full-Name" label="Full Name" type="text" />
+        <Input id="email" label="Email Address" type="email" />
+        <Input id="Street" label="Street" type="text" />
+        <div className="control-row">
+          <Input id="City" label="City" type="text" />
+          <Input id="Postal-Code" label="Postal Code" type="text" />
+        </div>
+        <p className="modal-actions">
+          <Button type="button" textOnly onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button>Confirm</Button>
+        </p>
+      </form>
     </Modal>
   );
 }
